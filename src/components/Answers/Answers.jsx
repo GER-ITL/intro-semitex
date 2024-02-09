6329e14adc6013404d036c6f8bd6cabeca7bce64
GER-ITL <danteryzvelt@inbox.ru>
2024-02-09 12:29:24 +0300

    update

--- src/components/Answers/Answers.jsx
+++ src/components/Answers/Answers.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { NavLink, Route, Routes } from 'react-router-dom'
+import data from '../../response'
 import s from './Answers.module.scss'
+
 const Answers = ({ children }) => {
 	return (
 		<div className={s.answers}>
@@ -24,36 +26,11 @@ const Answers = ({ children }) => {
 			</div>
 			<br />
 			<Routes>
-				<Route
-					path='/1'
-					element={
-						'				1  autem, officia voluptatibus dolorem? Omnis, illum deserunt adipisci perferendis laboriosam assumenda.'
-					}
-				/>
-				<Route
-					path='/2'
-					element={
-						'				2 Lutem, officia voluptatibus dolorem? Omnis, illum deserunt adipisci perferendis laboriosam assumenda.'
-					}
-				/>
-				<Route
-					path='/3'
-					element={
-						'				3 Corporis reiciendis voluptatum sapiente temporibus iste, blanditiis recusandae nam ducimus animi autem, officia voluptatibus dolorem? Omnis, illum deserunt adipisci perferendis laboriosam assumenda.'
-					}
-				/>
-				<Route
-					path='/4'
-					element={
-						'				4 Cimus animi autem, officia voluptatibus dolorem? Omnis, illum deserunt adipiscimus animi autem, officia voluptatibus dolorem? Omnis, illum deserunt adipisci perferendis laboriosam assumenda.'
-					}
-				/>
-				<Route
-					path='/5'
-					element={
-						'				5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis reiciendis voluptatum sapiente temporibus iste, blanditiis recusandae nam ducimus animi autem, officia voluptatibus dolorem? Omnis, illum deserunt adipisci perferendis laboriosam assumenda.'
-					}
-				/>
+				{data.map(answer => {
+					return (
+						<Route key={answer.id} path={answer.id} element={answer.answer} />
+					)
+				})}
 			</Routes>
 		</div>
 	)
---
+++ src/response.js
@@ -0,0 +1,23 @@
+const data = [
+	{
+		id: '/1',
+		answer:
+			'1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad libero iusto pariatur quia perferendis quibusdam eligendi iure perspiciatis non voluptatibus porro voluptas consequatur fugit, eius officia nam sed repudiandae sequi!',
+	},
+	{ id: '/2', answer: '2 Lorem ipsum dolor sit amet.' },
+	{
+		id: '/3',
+		answer: '3 Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+	},
+	{
+		id: '/4',
+		answer:
+			'4 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit earum aperiam deserunt quam, voluptatum quas.',
+	},
+	{
+		id: '/5',
+		answer:
+			'5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo id sapiente dolorem quia labore nostrum odit quisquam, eaque molestias quis.',
+	},
+]
+export default data
